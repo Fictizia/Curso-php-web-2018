7b3cb58163e23881d22b64bbf766e69ecb4251f2
natacha <natachamenjibar@gmail.com>
2018-12-14 22:02:20 +0100

    altertable

--- Natacha/Clase9/ejemploCrud/basedatos/shop.sql
+++ Natacha/Clase9/ejemploCrud/basedatos/shop.sql
@@ -22,9 +22,25 @@ CREATE TABLE `database`.`articulos`(
     `precio_rebajado` DECIMAL(9,2) NOT NULL, 
     PRIMARY KEY (`id`));
 
-    CREATE TABLE `clase9`.`lineas_pedido`(
+CREATE TABLE `clase9`.`lineas_pedido`(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `producto` VARCHAR(45) NOT NULL,
-    `precio` DECIMAL(9,2) NOT NULL, 
-    `precio_rebajado` DECIMAL(9,2) NOT NULL, 
-    PRIMARY KEY (`id`));
+    `id_pedido` INT NOT NULL,
+    `id_articulo` INT NOT NULL,
+    `precio_unidad` DECIMAL(9,2) NOT NULL, 
+    `unidades` INT NOT NULL, 
+    `precio_total` DECIMAL(9,2) NOT NULL, 
+    PRIMARY KEY (`id`),
+    INDEX (`id_pedido`),
+    FOREIGN KEY (`id_pedido`) REFERENCES `pedidos`(`id`),
+    INDEX (`id_articulo`),
+    FOREIGN KEY (`id_articulo`) REFERENCES `articulos`(`id`));
+
+
+    --alter TABLE
+
+ALTER TABLE `clase9`.`pets` ADD COLUMN `user_id` INT NOT NULL AFTER `id`,
+ALTER TABLE `clase9`.`pets` ADD FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ --
+ALTER TABLE `clase9`.`pets` ADD COLUMN `user_id` INT NOT NULL AFTER `id` SET DEFAULT '1';
+
+ALTER TABLE `clase9`.`pets` ALTER COLUMN `user_id` SET DEFAULT `1`;
